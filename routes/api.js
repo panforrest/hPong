194cc21da41a5e297c6e965ea3bb2e8f2f4cd2e8
panforrest <guoqianp@gmail.com>
2016-09-03 17:27:11 -0400

    apiDummyDataPullIndividualInvite

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var invites = [
+    {id:0, startTime:"09/03/2016 10:00:00", endTime:"09/03/2016 11:00:00", location:"Bryant Park"},
+    {id:1, startTime:"09/03/2016 10:30:00", endTime:"09/03/2016 11:30:00", location:"wang chen table tennis club"}
+]
+
 /* GET api page. */
 router.get('/:resource', function(req, res, next) {
-    var resource = req.params.resource
 
-    res.json({
-    	confirmation: 'success',
-    	resource: resource
-    })
+    var resource = req.params.resource
+    if (resource == 'invite') {
+        res.json({
+	    	confirmation: 'success',
+	    	invites: invites
+	    })
+    }
+});
+
+router.get('/:resource/:id', function(req, res, next) {
+
+    var resource = req.params.resource
+    var id = req.params.id
+    if (resource == 'invite') {
+    	var invite = invites[id]
+
+        res.json({
+	    	confirmation: 'success',
+	    	invite: invite
+	    })
+    }
 });
 
 module.exports = router;
